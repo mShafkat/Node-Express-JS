ab96fb6f13779ca1ad879df67b1587fa2958b470
mShafkat <shafkatlabib2001@gmail.com>
2025-10-02 07:49:46 +0600

    added json in index

--- first project/index.js
+++ first project/index.js
@@ -7,11 +7,24 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello</h1>");
+    const users = [
+        {id: 1 , name: 'x'},
+        {id: 2 , name: 'y'}
+    ]
+
+    res.json(users)
+        
 });
 
-app.get('/user/:userid/book/:bookid', (req, res) => {
-    res.send("Book ID: " + req.params.bookid + ", User ID: " + req.params.userid);
-});
+// app.get('/user/:userid/book/:bookid', (req, res) => {
+//     res.send("Book ID: " + req.params.bookid + ", User ID: " + req.params.userid);
+// });
+
+// app.get('/search', (req, res) => {
+//     const name = req.query.name;
+//     const age = req.query.age;
+//     res.send(`Search results for name : ${name}, age : ${age}`);
+// });
+
 
 
